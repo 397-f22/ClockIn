bf5270d1ae437fd1d983a4ca9b0a505a5763ca25
Christopher Song <chris4728.song2001@gmail.com>
2022-11-20 17:42:10 -0600

    Small refactor for mock data

---
+++ src/utils/mockAlarmList.js
@@ -0,0 +1,22 @@
+export const mockAlarmList = [
+  {
+      "hour": "10",
+      "minute": "00",
+      "active": true
+  },
+  {
+      "hour": "9",
+      "minute": "15",
+      "active": true
+  },
+  {
+      "hour": "6",
+      "minute": "30",
+      "active": true
+  },
+  {
+    "hour": "7",
+    "minute": "0",
+    "active": true
+  }
+];

d2d0baf002703ecd9050d83b16ff5a3ffa22f9a0
Christopher Song <chris4728.song2001@gmail.com>
2022-11-20 18:27:20 -0600

    Styling updates to SetAlarm component

--- src/components/SetAlarm.jsx
+++ src/components/SetAlarm.jsx
@@ -1,3 +1,5 @@
+import "./SetAlarm.css";
+
 const SetAlarm = ({ alarmList, setAlarmList }) => {
   const createAlarm = (e) => {
     e.preventDefault();
@@ -9,32 +11,39 @@ const SetAlarm = ({ alarmList, setAlarmList }) => {
     };
     console.log(alarm)
     setAlarmList([...alarmList, alarm]);
-
-    console.log(alarmList)
-
   }
 
   return (
     <form onSubmit={createAlarm}>
       <div className="setAlarm">
-        <select>
-          {
-            [...Array(24).keys()].map((hour) => (
-              <option key={`hour-${hour}`} value={hour}>{String(hour).padStart(2, '0')}</option>
-            ))
-          }
-        </select>
-        <select >
-          {
-            [...Array(60).keys()].map((minute) => (
-              <option key={`minute-${minute}`} value={minute}>{String(minute).padStart(2, '0')}</option>
-            ))
-          }
-        </select>
+        <div className="setAlarmColumns">
+          <div className="setAlarmColumn">
+            <div>H</div>
+            <select className="setAlarmSelect">
+              {
+                [...Array(24).keys()].map((hour) => (
+                  <option key={`hour-${hour}`} value={hour}>{String(hour).padStart(2, '0')}</option>
+                ))
+              }
+            </select>
+          </div>
+          <div className="setAlarmColumn">
+            <div>M</div>
+            <select className="setAlarmSelect">
+              {
+                [...Array(60).keys()].map((minute) => (
+                  <option key={`minute-${minute}`} value={minute}>{String(minute).padStart(2, '0')}</option>
+                ))
+              }
+            </select>
+          </div>
+        </div>
+        <div>
+          <button className="alarmSubmit" type="submit">
+            Set Alarm
+          </button>
+        </div>
       </div>
-      <button className="alarmSubmit" type="submit" >
-        Set Alarm
-      </button>
     </form>)
 };
 

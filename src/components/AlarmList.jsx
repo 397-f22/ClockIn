75c77973852f4db77a720fdc9d536454885b4d98
Adam Stewart <adamjoesph11@gmail.com>
2022-11-20 22:19:43 -0600

    removed dead comment code hope it didn't break stuff

--- src/components/AlarmList.jsx
+++ src/components/AlarmList.jsx
@@ -14,37 +14,7 @@ const AlarmList = () => {
       return parseInt(alarmObj.hour) === currentHour && parseInt(alarmObj.minute) == currentMinute;
     };
 
-    // useEffect(() => {
-    //   const currentDatetime = new Date();
-    //   const alarm = document.getElementById("alarm");
-    // //   alarm.muted = true;
-    // //   alarm.play();
-
-    //   for (let i = 0; i < alarmList.length; i++) {
-    //     if (!alarmList[i].active) {
-    //       continue;
-    //     };
-
-    //     if (alarmList[i].ringing) {
-    //       continue;
-    //     }
-
-    //     if (alarmShouldExpire(alarmList[i], currentDatetime.getHours(), currentDatetime.getMinutes())) {
-    //       document.addEventListener("click", () => {
-    //         alarm.play();
-    //       });
-
-    //       setAlarmList([
-    //         ...alarmList.slice(0, i),
-    //         {
-    //           ...alarmList[i],
-    //           ringing: true
-    //         },
-    //         ...alarmList.slice(i + 1)
-    //       ]);
-    //     };
-    //   };
-    // }, []);
+   
 
     useEffect(() => {
       if (alarmList.every(alarm => !alarm.ringing)) {

33476df88e1a3293f3eb5ddd93d8979ee0c888f4
Adam Stewart <adamjoesph11@gmail.com>
2022-12-05 19:49:42 -0600

    code cleanup

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -8,10 +8,6 @@ describe('A user should not be able to change the puzzle type while any alarm is
     vi.useFakeTimers()
   })
   test("puzzleMode should stay the same while an alarm is ringing", () => {
-    //Check if alarm is ringing because of mock alarmList
-    //Check if current puzzle mode is correct
-    //CHeck if changing puzzle mode work from puzzleModeSlider
-    //Add display none value to AlarmList that says whether or not it is true
     let alarm1 = {
       "hour": 12,
       "minute": 12,
@@ -95,72 +91,15 @@ describe('A user should be able to add an alarm to their list of alarms and have
       render(<AlarmList currentUser={currentUser} alarms={alarmList} testing />);
     })
     const submit = screen.getByTestId('submit')
-    // act (() => {
-    //   vi.advanceTimersByTime(4000)
-    // })
-    //act (() => {
-    //})
     act( () => {
       fireEvent.submit(submit,{target: [{value: alarm3.hour},{value: alarm3.minute}, {value: 'AM'}]})
 
     })
 
     await act(async () => { vi.runOnlyPendingTimers() });
-    // const listLength = screen.getByTestId('alarm-list')
-    // expect(listLength).equal(3)
     expect(screen.findByText(/2:14 PM/i))
-    // await screen.findByText(/1:13 PM/i)
-
-    // await act (async () => {
-    //   vi.advanceTimersByTime(4000)
-    //   await screen.findByText(/2:14 PM/i)
-      
-    // })
-    // let alarmList2 = [...alarmList,alarm3]
-    // alarmList2.push(alarm3)
-    // console.log(alarmList2)
-    // act (() => {
-    //   render(<AlarmList currentUser={currentUser} alarms={alarmList2} testing />)
-    // })
-    //let result = await screen.findByText(/2:14 PM/i)
-    // await waitFor( () => {
-    //   expect(screen.getByText(/2:14 PM/i)).toBeInTheDocument()
-    // })
-    //expect(3).toEqual(3)//.toBeInTheDocument();
-
-    // const ringing2 = screen.getByTestId('ringing')
-    // expect(ringing2.textContent).toBe('true')
 
   })
 })
 
-// describe('counter tests', () => {
-
-//   test("Counter should be 0 at the start", () => {
-//     render(<App />);
-//     expect(2).toBe(2)
-//   });
-
-//   test("Counter should increment by one when clicked", async () => {
-//     render(<App />);
-//     expect(3).toBe(3)
-//   });
-
-// });
-
-// describe('counter tests', () => {
-    
-//   test("Counter should be 0 at the start", () => {
-//     render(<App />);
-//     expect(screen.getByText('count is: 0')).toBeDefined();
-//   });
-
-//   test("Counter should increment by one when clicked", async () => {
-//     render(<App />);
-//     const counter = screen.getByRole('button');
-//     fireEvent.click(counter);
-//     expect(await screen.getByText('count is: 1')).toBeDefined();
-//   });
-
-// });
 
